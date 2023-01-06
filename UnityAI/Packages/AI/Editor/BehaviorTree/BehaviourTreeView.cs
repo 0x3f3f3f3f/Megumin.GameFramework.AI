aca1b71f40cf9bfc13c2929a9fb0fa54be1e3f20
云却 <479813005@qq.com>
2023-01-06 18:53:34 +0800

    增加背景网格

--- UnityAI/Packages/AI/Editor/BehaviorTree/BehaviourTreeView.cs
+++ UnityAI/Packages/AI/Editor/BehaviorTree/BehaviourTreeView.cs
@@ -15,13 +15,30 @@ namespace Megumin.GameFramework.AI.BehaviorTree.Editor
         {
             GridBackground element = new GridBackground();
             Insert(0, element);
+
             MiniMap child = new MiniMap();
+            child.name = "minimap";
             this.Add(child);
+
             //child.SetPosition(Rect.zero);
+            AddNode();
+
+            this.AddManipulator(new ContentZoomer());
+            this.AddManipulator(new ContentDragger());
+            //this.AddManipulator(new DoubleClickSelection());
+            this.AddManipulator(new SelectionDragger());
+            this.AddManipulator(new RectangleSelector());
+
+            //this.SetupZoom(0.5f, 2f,0.8f,1f);
+        }
+
+        public void AddNode()
+        {
             Node child1 = new Node() { name = "testNode" };
+            child1.title = "TestNode";
             this.Add(child1);
-            child1.capabilities |= Capabilities.Movable;
-            child1.SetPosition(new Rect(100,100,100,100));
+            //child1.capabilities |= Capabilities.Movable;
+            child1.SetPosition(new Rect(100, 100, 100, 100));
         }
     }
 }
