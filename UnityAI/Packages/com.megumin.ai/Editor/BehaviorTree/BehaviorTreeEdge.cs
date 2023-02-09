980604858a0624b9a641b642a426de5a55ffea53
云却 <479813005@qq.com>
2023-02-09 17:16:30 +0800

    连接子节点

---
+++ UnityAI/Packages/com.megumin.ai/Editor/BehaviorTree/BehaviorTreeEdge.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor.Experimental.GraphView;
+
+namespace Megumin.GameFramework.AI.BehaviorTree.Editor
+{
+    internal class BehaviorTreeEdge : Edge
+    {
+    }
+}
--- UnityAI/Packages/com.megumin.ai/Editor/BehaviorTree/BehaviorTreeView.cs
+++ UnityAI/Packages/com.megumin.ai/Editor/BehaviorTree/BehaviorTreeView.cs
@@ -8,6 +8,7 @@ using System;
 using UnityEditor;
 using Megumin.GameFramework.AI.Editor;
 using System.Linq;
+using PlasticPipe.PlasticProtocol.Server.Stubs;
 
 namespace Megumin.GameFramework.AI.BehaviorTree.Editor
 {
@@ -94,7 +95,12 @@ namespace Megumin.GameFramework.AI.BehaviorTree.Editor
                 {
                     foreach (var edge in graphViewChange.edgesToCreate)
                     {
-                        Debug.Log($"Create Edge {edge.input.node.name}");
+                        if (edge.input.node is BehaviorTreeNodeView childNodeView &&
+                            edge.output.node is BehaviorTreeNodeView parentNodeView)
+                        {
+                            Debug.Log($"Create Edge {edge.input.node.name}");
+                            ConnectChild(parentNodeView,childNodeView);
+                        }
                     }
                 }
             }
@@ -374,6 +380,29 @@ namespace Megumin.GameFramework.AI.BehaviorTree.Editor
             UndoRecord("Change Start Node");
             Tree.StartNode = behaviorTreeNodeView.SONode.Node;
             behaviorTreeNodeView.AddToClassList(StartNodeClass);
+        }
+
+        private void ConnectChild(BehaviorTreeNodeView parentNodeView, BehaviorTreeNodeView childNodeView)
+        {
+            this.LogFuncName();
+            UndoRecord($"ConnectChild [{parentNodeView.SONode.name}] -> [{childNodeView.SONode.name}]");
+            if (parentNodeView.SONode.Node is BTParentNode parentNode)
+            {
+                parentNode.children.Add(childNodeView.SONode.Node);
+
+                //重新排序
+                SortChild(parentNode);
+            }
+        }
+
+        public void SortChild(BTParentNode parentNode)
+        {
+            parentNode.children.Sort((lhs, rhs) =>
+            {
+                var lhsView = GetElementByGuid(lhs.GUID);
+                var rhsView = GetElementByGuid(rhs.GUID);
+                return lhsView.layout.position.x.CompareTo(rhsView.layout.position.x);
+            });
         }
     }
 
