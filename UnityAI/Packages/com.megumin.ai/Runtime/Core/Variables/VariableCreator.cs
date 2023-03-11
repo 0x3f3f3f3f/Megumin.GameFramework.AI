08e7719293b114c023a116097f8e86ca1379275b
云却 <479813005@qq.com>
2023-03-11 21:26:40 +0800

    参数移动到Core

--- UnityAI/Packages/com.megumin.ai/Runtime/Core/Variables/VariableCreator.cs
+++ UnityAI/Packages/com.megumin.ai/Runtime/Core/Variables/VariableCreator.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace Megumin.GameFramework.AI.BehaviorTree
+namespace Megumin.GameFramework.AI
 {
     public class VariableCreator
     {
--- UnityAI/Packages/com.megumin.ai/Runtime/Core/Variables/VariableTable.cs
+++ UnityAI/Packages/com.megumin.ai/Runtime/Core/Variables/VariableTable.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Megumin.GameFramework.AI.BehaviorTree
+namespace Megumin.GameFramework.AI
 {
     [Serializable]
     public class VariableTable
