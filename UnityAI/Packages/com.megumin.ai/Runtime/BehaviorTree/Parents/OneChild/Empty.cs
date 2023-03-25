ec952d171bd82efa36287f9a1f2d3c25a9886b81
云却 <479813005@qq.com>
2023-03-25 18:52:02 +0800

    修改空节点

--- UnityAI/Packages/com.megumin.ai/Runtime/BehaviorTree/Parents/OneChild/Empty.cs
+++ UnityAI/Packages/com.megumin.ai/Runtime/BehaviorTree/Parents/OneChild/Empty.cs
@@ -8,10 +8,14 @@ using System.Threading.Tasks;
 namespace Megumin.GameFramework.AI.BehaviorTree
 {
     [Category("Debug")]
-    public sealed class Empty : ActionTaskNode
+    public sealed class Empty : OneChildNode
     {
         protected override Status OnTick(BTNode from)
         {
+            if (Child0 != null)
+            {
+                return Child0.Tick(this);
+            }
             return GetIgnoreResult(from);
         }
     }
