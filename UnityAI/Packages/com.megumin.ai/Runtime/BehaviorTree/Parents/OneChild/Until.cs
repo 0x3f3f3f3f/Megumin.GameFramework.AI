f2d475b28851f63e24f243a03a80629879935cf8
云却 <479813005@qq.com>
2023-03-31 10:24:51 +0800

    增加Until父节点

---
+++ UnityAI/Packages/com.megumin.ai/Runtime/BehaviorTree/Parents/OneChild/Until.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Megumin.GameFramework.AI.BehaviorTree
+{
+    public class Until : OneChildNode, IDetailable
+    {
+        public Status DesState = Status.Succeeded;
+        protected override Status OnTick(BTNode from)
+        {
+            if (Child0 == null)
+            {
+                return GetIgnoreResult(from);
+            }
+
+            var res = Child0.Tick(this);
+
+            if (res == DesState)
+            {
+                return res;
+            }
+
+            return Status.Running;
+        }
+
+        public string GetDetail()
+        {
+            return DesState.ToString();
+        }
+    }
+}
