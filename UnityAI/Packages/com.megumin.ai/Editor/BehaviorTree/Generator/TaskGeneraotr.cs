2e8ebf7245be8c82b531ee70620dffc787311070
云却 <479813005@qq.com>
2023-04-16 15:35:54 +0800

    生成组件节点代码

--- UnityAI/Packages/com.megumin.ai/Editor/BehaviorTree/Generator/TaskGeneraotr.cs
+++ UnityAI/Packages/com.megumin.ai/Editor/BehaviorTree/Generator/TaskGeneraotr.cs
@@ -1,8 +1,9 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using Unity.VisualScripting.YamlDotNet.Serialization;
 using UnityEditor;
@@ -19,9 +20,21 @@ namespace Megumin.GameFramework.AI.BehaviorTree.Editor
         public void Generate()
         {
             Type type = typeof(NavMeshAgent);
-            var mes = type.GetMethods(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
+            GenerateType(type);
 
-            foreach (var m in mes)
+            AssetDatabase.Refresh();
+        }
+
+        public void GenerateType(Type type)
+        {
+            if (!type.IsSubclassOf(typeof(UnityEngine.Component)))
+            {
+                return;
+            }
+
+            var methods = type.GetMethods(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance).ToList();
+
+            foreach (var m in methods)
             {
                 Debug.Log(m.ToStringReflection());
 
@@ -35,22 +48,34 @@ namespace Megumin.GameFramework.AI.BehaviorTree.Editor
                     continue;
                 }
 
-
+                var ob = m.GetCustomAttribute<ObsoleteAttribute>();
+                if (ob != null)
+                {
+                    continue;
+                }
 
                 CSCodeGenerator codeGenerator = new CSCodeGenerator();
-                string className = GenerateCode(type, m, codeGenerator);
+                var success = GenerateCode(type, m, codeGenerator);
 
-                var fileName = $"{className}.cs";
-                var dir = AssetDatabase.GetAssetPath(OutputFolder);
-                string filePath = Path.Combine(dir, fileName);
-                var path = Path.GetFullPath(filePath);
-                codeGenerator.Generate(path);
+                if (success)
+                {
+                    var fileName = $"{type.Name}_{m.Name}.cs";
+                    var dir = AssetDatabase.GetAssetPath(OutputFolder);
+
+                    dir = Path.Combine(dir, type.Name);
+                    if (!Directory.Exists(dir))
+                    {
+                        Directory.CreateDirectory(dir);
+                    }
+
+                    string filePath = Path.Combine(dir, fileName);
+                    var path = Path.GetFullPath(filePath);
+                    codeGenerator.Generate(path);
+                }
             }
-
-            AssetDatabase.Refresh();
         }
 
-        private string GenerateCode(Type type, MethodInfo m, CSCodeGenerator generator)
+        private bool GenerateCode(Type type, MethodInfo method, CSCodeGenerator generator)
         {
             generator.Push($"using System.Collections;");
             generator.Push($"using System.Collections.Generic;");
@@ -61,20 +86,63 @@ namespace Megumin.GameFramework.AI.BehaviorTree.Editor
             generator.Push($"namespace Megumin.GameFramework.AI.BehaviorTree");
             using (generator.NewScope)
             {
+                generator.Push($"[Category(\"Unity/{type.Name}\")]");
                 generator.Push($"public class $(ClassName) : BTActionNode<$(ComponentName)>");
                 using (generator.NewScope)
                 {
+                    //声明参数
+                    var @params = method.GetParameters();
+                    foreach (var param in @params)
+                    {
+                        if (TryGetParamType(param, out var paramType))
+                        {
+                            generator.Push($"public {paramType.FullName} {param.Name};");
+                        }
+                        else
+                        {
+                            //参数类型不支持这个方法，不能生成节点。
+                            return false;
+                        }
+                    }
 
+                    generator.PushBlankLines();
+                    generator.Push($"protected override Status OnTick(BTNode from, object options = null)");
+                    using (generator.NewScope)
+                    {
+                        //MyAgent.CalculatePath(targetPosition, path);
+                        var callString = $"MyAgent.{method.Name}(";
+                        for (int i = 0; i < @params.Length; i++)
+                        {
+                            callString += $"{@params[i].Name}";
+                            if (i < @params.Length - 1)
+                            {
+                                callString += ", ";
+                            }
+                        }
+                        callString += ");";
+
+                        generator.Push(callString);
+                        generator.Push($"return Status.Succeeded;");
+                    }
                 }
             }
 
             generator.PushBlankLines(4);
 
-            var className = $"{type.Name}_{m.Name}";
+            var className = $"{type.Name}_{method.Name}";
             generator.Macro["$(ClassName)"] = className;
             generator.Macro["$(ComponentName)"] = type.FullName;
-            return className;
+            return true;
         }
 
+        private bool TryGetParamType(ParameterInfo param, out Type type)
+        {
+            type = param.ParameterType;
+            if (param.IsOut)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
