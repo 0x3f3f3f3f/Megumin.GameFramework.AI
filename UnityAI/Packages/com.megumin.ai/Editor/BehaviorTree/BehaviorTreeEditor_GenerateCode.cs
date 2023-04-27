58c6e461ac90ba2a9ea03c3b4b9717d50f4b26e1
云却 <479813005@qq.com>
2023-04-27 13:52:25 +0800

    根据树实例生成代码

--- UnityAI/Packages/com.megumin.ai/Editor/BehaviorTree/BehaviorTreeEditor_GenerateCode.cs
+++ UnityAI/Packages/com.megumin.ai/Editor/BehaviorTree/BehaviorTreeEditor_GenerateCode.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEditor;
-using Megumin;
+﻿using System.Collections.Generic;
+using Megumin.Reflection;
 using Megumin.Serialization;
-using static Megumin.Reflection.TypeCache;
+using UnityEditor;
 
 namespace Megumin.GameFramework.AI.BehaviorTree.Editor
 {
@@ -15,7 +10,14 @@ namespace Megumin.GameFramework.AI.BehaviorTree.Editor
         //TODO, 重构代码，不然后面还要改
         public void GenerateCode()
         {
-            BehaviorTreeAsset_1_1 behaviorTree = CurrentAsset.AssetObject as BehaviorTreeAsset_1_1;
+            //BehaviorTreeAsset_1_1 behaviorTree = CurrentAsset.AssetObject as BehaviorTreeAsset_1_1;
+            BehaviorTree tree = TreeView.Tree;
+
+            if (tree == null)
+            {
+                return;
+            }
+
             CSCodeGenerator generator = new();
 
 
@@ -29,16 +31,15 @@ namespace Megumin.GameFramework.AI.BehaviorTree.Editor
                 generator.Push($"public sealed partial class $(ClassName) : BehaviorTreeCreator");
                 using (generator.NewScope)
                 {
-                    generator.Push($"static readonly Unity.Profiling.ProfilerMarker instantiateMarker = new(\"$(TreeName)_Init\");");
-                    GenerateInitMethod(generator, behaviorTree);
+                    GenerateInitMethod(generator, tree);
                 }
             }
 
             generator.Macro["$(ClassName)"] =
-                BehaviorTreeCreator.GetCreatorTypeName(behaviorTree.TreeName, behaviorTree.GUID);
-            generator.Macro["$(TreeName)"] = behaviorTree.TreeName;
+                BehaviorTreeCreator.GetCreatorTypeName(tree.Asset.name, tree.GUID);
+            generator.Macro["$(TreeName)"] = tree.Asset.name;
 
-            string filePath = $"Assets/{behaviorTree.name}_Gene.cs";
+            string filePath = $"Assets/{tree.Asset.name}_Gene.cs";
             generator.Generate(filePath);
 
             //Open
@@ -49,6 +50,7 @@ namespace Megumin.GameFramework.AI.BehaviorTree.Editor
 
         public void GenerateInitMethod(CSCodeGenerator generator, BehaviorTreeAsset_1_1 behaviorTree)
         {
+            generator.Push($"static readonly Unity.Profiling.ProfilerMarker instantiateMarker = new(\"$(TreeName)_Init\");");
             generator.Push($"public override BehaviorTree Instantiate(InitOption initOption, IRefFinder refFinder = null)");
             using (generator.NewScope)
             {
@@ -94,52 +96,199 @@ namespace Megumin.GameFramework.AI.BehaviorTree.Editor
                 generator.Push("//反序列化 nodes");
                 foreach (var item in behaviorTree.nodes)
                 {
-                    using (generator.GetRegionScope(item.Name))
-                    {
-                        var varName = SafeVarName(item.Name);
-                        if (item.Member != null)
-                        {
-                            foreach (var memberData in item.Member)
-                            {
-                                using (generator.NewScope)
-                                {
-                                    generator.Push($"//SetMember {memberData.Name}");
-                                    if (memberData.Type == ObjectData.NullType)
-                                    {
-                                        generator.Push($"{varName}.{memberData.Name} = null;");
-                                    }
-                                    else if (memberData.Type == ObjectData.RefType)
-                                    {
-                                        var memberVarName = SafeVarName(memberData.Value);
-                                        generator.Push($"{varName}.{memberData.Name} = {memberVarName};");
-                                    }
-                                    else
-                                    {
-                                        if (StringFormatter.TryDeserialize(memberData.Type, memberData.Value, out var value))
-                                        {
-                                            if (value is string)
-                                            {
-                                                generator.Push($"{varName}.{memberData.Name} = \"{value}\";");
-                                            }
-                                            else
-                                            {
-                                                generator.Push($"{varName}.{memberData.Name} = {value};");
-                                            }
-                                        }
-                                        else
-                                        {
-                                            generator.Push($"//{memberData.Type} can not parse!");
-                                        }
-                                    }
-                                }
-                                generator.PushBlankLines();
-                            }
-                        }
-                    }
+                    Deserialize(generator, item);
                 }
                 generator.PushBlankLines();
 
+                generator.Push("//反序列化 decorators");
+                foreach (var item in behaviorTree.decorators)
+                {
+                    Deserialize(generator, item);
+                }
+                generator.PushBlankLines();
+
+                //generator.Push("//反序列化 ref obj");
+                //foreach (var item in behaviorTree.refObjs)
+                //{
+                //    Deserialize(generator, item);
+                //}
+                //generator.PushBlankLines();
+
                 generator.Push($"return tree;");
+            }
+        }
+
+        public void GenerateInitMethod(CSCodeGenerator generator, BehaviorTree tree)
+        {
+            generator.Push($"static readonly Unity.Profiling.ProfilerMarker instantiateMarker = new(\"$(TreeName)_Init\");");
+            generator.Push($"public override BehaviorTree Instantiate(InitOption initOption, IRefFinder refFinder = null)");
+            using (generator.NewScope)
+            {
+                generator.Push($"using var profiler = instantiateMarker.Auto();");
+                generator.PushBlankLines();
+
+                generator.Push($"if (initOption == null)");
+                using (generator.NewScope)
+                {
+                    generator.Push($"return null;");
+                }
+                generator.PushBlankLines();
+
+                generator.Push($"BehaviorTree tree = new();");
+                generator.Push($"tree.GUID = \"{tree.GUID}\";");
+                generator.Push($"tree.RootTree = tree;");
+                generator.Push($"tree.InitOption = initOption;");
+                generator.PushBlankLines();
+
+                Dictionary<object, string> declaredObj = new();
+                Queue<(object, string)> needSetMember = new();
+
+                //缓存所有已知引用对象
+                declaredObj.Add(tree, SafeVarName(tree.GUID));
+
+
+
+                void DeclareObj(string varName, object obj)
+                {
+                    if (declaredObj.TryGetValue(obj, out var variableName))
+                    {
+                        if (varName != variableName)
+                        {
+                            generator.Push($"var {varName} = {variableName};");
+                            declaredObj.Add(obj, varName);
+                        }
+                    }
+                    else
+                    {
+                        generator.Push($"var {varName} = new {obj.GetType().ToCodeString()}();");
+                        declaredObj.Add(obj, varName);
+
+                        needSetMember.Enqueue((obj, varName));
+                    }
+                }
+
+                void DeclareObjMember(string varName, object obj)
+                {
+                    foreach (var (memberName, memberValue, memberType) in obj.GetSerializeMembers())
+                    {
+                        if (memberType.IsPrimitive || memberValue is string || memberValue == null)
+                        {
+                            //generator.Push($"{varName}.{memberName} = {memberValue.ToCodeString()};");
+                        }
+                        else
+                        {
+                            //引用对象声明
+                            var varMemberName = SafeVarName($"{varName}.{memberName}");
+                            DeclareObj(varMemberName, memberValue);
+                        }
+                    }
+                }
+
+                generator.Push("//声明复杂对象");
+                foreach (var variable in tree.Variable.Table)
+                {
+                    string varName = SafeVarName(variable.RefName);
+                    DeclareObj(varName, variable);
+                    DeclareObjMember(varName, variable);
+                    generator.PushBlankLines();
+                }
+
+                foreach (var node in tree.AllNodes)
+                {
+                    string varName = SafeVarName(node.GUID);
+                    DeclareObj(varName, node);
+                    DeclareObjMember(varName, node);
+
+                    foreach (var decorator in node.Decorators)
+                    {
+                        string varName1 = SafeVarName(decorator.GUID);
+                        DeclareObj(varName1, decorator);
+                        DeclareObjMember(varName1, decorator);
+                    }
+
+                    generator.PushBlankLines();
+                }
+
+                HashSet<object> alrendySetMember = new();
+                generator.Push("//生成member代码");
+
+                while (needSetMember.Count > 0)
+                {
+                    var v = needSetMember.Dequeue();
+                    var item = v.Item1;
+                    var varName = v.Item2;
+
+                    if (alrendySetMember.Contains(item))
+                    {
+                        continue;
+                    }
+
+                    foreach (var (memberName, memberValue, memberType) in item.GetSerializeMembers())
+                    {
+                        if (memberType.IsPrimitive || memberValue is string || memberValue == null)
+                        {
+                            generator.Push($"{varName}.{memberName} = {memberValue.ToCodeString()};");
+                        }
+                        else
+                        {
+                            //引用对象声明
+                            if (declaredObj.TryGetValue(memberValue, out var dName))
+                            {
+                                generator.Push($"{varName}.{memberName} = {dName};");
+                            }
+                            else
+                            {
+                                generator.Push($"//TODO : {memberName}");
+                            }
+                        }
+                    }
+
+                    alrendySetMember.Add(item);
+
+                    generator.PushBlankLines();
+                }
+
+                generator.Push($"return tree;");
+            }
+        }
+
+        public void Deserialize(CSCodeGenerator generator, ObjectData item)
+        {
+            using (generator.GetRegionScope(item.Name))
+            {
+                var varName = SafeVarName(item.Name);
+                if (item.Member != null)
+                {
+                    foreach (var memberData in item.Member)
+                    {
+                        using (generator.NewScope)
+                        {
+                            generator.Push($"//SetMember {memberData.Name}");
+
+                            var resultString = $"//{memberData.Type} can not parse!";
+
+                            if (memberData.Type == ObjectData.NullType)
+                            {
+                                resultString = "null";
+                            }
+                            else if (memberData.Type == ObjectData.RefType)
+                            {
+                                resultString = SafeVarName(memberData.Value);
+                            }
+                            else
+                            {
+                                if (StringFormatter.TryDeserialize(memberData.Type, memberData.Value, out var value))
+                                {
+                                    resultString = value.ToCodeString();
+                                }
+                            }
+
+                            generator.Push($"{varName}.{memberData.Name} = {resultString};");
+                        }
+
+                        generator.PushBlankLines();
+                    }
+                }
             }
         }
 
@@ -163,7 +312,12 @@ namespace Megumin.GameFramework.AI.BehaviorTree.Editor
 
         public string SafeVarName(string refName)
         {
-            var name = $"temp_{refName}";
+            var name = refName;
+            if (name.StartsWith("temp_") == false)
+            {
+                name = $"temp_{refName}";
+            }
+
             name = name.Replace('-', '_');
             name = name.Replace('.', '_');
             return name;
