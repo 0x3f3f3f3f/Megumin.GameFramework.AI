cdbf1e2fce214c929fd7b1722df00b9bc71c0c5f
云却 <479813005@qq.com>
2023-09-09 20:33:07 +0800

    增加节点图标

--- UnityAI/Packages/com.megumin.ai.behaviortree/Runtime/Gameplay/CanSeeTarget.cs
+++ UnityAI/Packages/com.megumin.ai.behaviortree/Runtime/Gameplay/CanSeeTarget.cs
@@ -7,9 +7,14 @@ using Megumin.AI;
 using Megumin.AI.BehaviorTree;
 using Megumin.Perception;
 using UnityEngine;
+using System.ComponentModel;
 
 namespace Megumin.AI.BehaviorTree
 {
+    [Icon("d_viewtoolorbit on@2x")]
+    [Description("TransformPerception CanSeeTarget ")]
+    [Category("Gameplay")]
+    [AddComponentMenu("CanSeeTarget(Transform)")]
     public class CanSeeTarget : ConditionDecorator<TransformPerception>
     {
         public RefVar_Transform Target;
