07188b96927b39c6b2b03ce0e03c34628b5bb60d
云却 <479813005@qq.com>
2023-09-13 20:38:59 +0800

    巡逻增加距离当前位置的最小距离

--- UnityAI/Packages/com.megumin.ai.behaviortree/Runtime/Gameplay/Patrol.cs
+++ UnityAI/Packages/com.megumin.ai.behaviortree/Runtime/Gameplay/Patrol.cs
@@ -75,6 +75,9 @@ namespace Megumin.AI.BehaviorTree
         public float MaxRange = 10f;
         public float MinRange = 2f;
 
+        [Space]
+        public float MinDistance2Current = 4f;
+
         Vector3 start;
 
         protected override void OnEnter(object options = null)
@@ -104,8 +107,7 @@ namespace Megumin.AI.BehaviorTree
 
         public bool TryMoveNext()
         {
-            var random = Random.insideUnitCircle * Random.Range(MinRange, MaxRange);
-            var next = start + new Vector3(random.x, 0, random.y);
+            Vector3 next = RandomNext(start);
 
             if (MyAgent.MoveTo(next))
             {
@@ -113,6 +115,22 @@ namespace Megumin.AI.BehaviorTree
                 return true;
             }
             return false;
+        }
+
+        public Vector3 RandomNext(Vector3 orignal)
+        {
+            int count = 0;
+            while (true)
+            {
+                var random = Random.insideUnitCircle * Random.Range(MinRange, MaxRange);
+                var next = orignal + new Vector3(random.x, 0, random.y);
+                count++;
+                if ((Transform.position - next).magnitude > MinDistance2Current || count > 20)
+                {
+                    //随机的下一个点 必须与当前位置 距离足够远
+                    return next;
+                }
+            }
         }
     }
 }
