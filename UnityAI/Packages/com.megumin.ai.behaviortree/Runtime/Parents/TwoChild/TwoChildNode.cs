21d7055446e7d19631cc494af6e7c13a0cabffff
云却 <479813005@qq.com>
2023-09-19 19:39:24 +0800

    重构，父节点拆分为独立文件

---
+++ UnityAI/Packages/com.megumin.ai.behaviortree/Runtime/Parents/TwoChild/TwoChildNode.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Megumin.AI.BehaviorTree
+{
+    public abstract class TwoChildNode : BTParentNode
+    {
+        public BTNode Child0
+        {
+            get
+            {
+                if (Children.Count > 0)
+                {
+                    return Children[0];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public BTNode Child1
+        {
+            get
+            {
+                if (Children.Count > 1)
+                {
+                    return Children[1];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        protected override void OnAbort(object options = null)
+        {
+            Child0.Abort(this, options);
+        }
+    }
+
+
+
+    public abstract class TwoChildNode<T> : BTParentNode<T>
+    {
+        public BTNode Child0
+        {
+            get
+            {
+                if (Children.Count > 0)
+                {
+                    return Children[0];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public BTNode Child1
+        {
+            get
+            {
+                if (Children.Count > 1)
+                {
+                    return Children[1];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        protected override void OnAbort(object options = null)
+        {
+            Child0.Abort(this, options);
+        }
+    }
+}
+
+
+
+
