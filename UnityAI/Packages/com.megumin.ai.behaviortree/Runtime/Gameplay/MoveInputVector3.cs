3935c4036b6ebab27cec59529452034107803042
云却 <479813005@qq.com>
2023-09-25 21:42:20 +0800

    重构巡逻

---
+++ UnityAI/Packages/com.megumin.ai.behaviortree/Runtime/Gameplay/MoveInputVector3.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+namespace Megumin.AI.BehaviorTree
+{
+    [Icon("d_navmeshdata icon")]
+    [DisplayName("MoveTo")]
+    [Description("IMoveInputable<Vector3>")]
+    [Category("Gameplay")]
+    [AddComponentMenu("MoveTo(IMoveInputable<Vector3>)")]
+    [HelpURL(URL.WikiTask + "MoveInputVector3")]
+    public class MoveInputVector3 : BTActionNode<IMoveInputable<Vector3>>
+    {
+        [Space]
+        public float StopingDistance = 0.25f;
+
+        public bool IgnoreYAxis = true;
+
+
+        /// <summary>
+        /// 移动过程中目的地改变，自动重新设置目的地
+        /// </summary>
+        [Space]
+        public bool KeepDestinationNew = false;
+
+        [Space]
+        public Destination destination;
+
+        protected Vector3 Last;
+
+        protected override void OnEnter(object options = null)
+        {
+            base.OnEnter(options);
+            Last = GetDestination();
+            GetLogger()?.WriteLine($"MoveTo MyAgent : {MyAgent}  Des : {destination?.Dest_Transform?.Value.name} Last:{Last}");
+        }
+
+        protected Vector3 GetDestination()
+        {
+            return destination.GetDestination();
+        }
+
+        protected override Status OnTick(BTNode from, object options = null)
+        {
+            if (KeepDestinationNew)
+            {
+                Last = GetDestination();
+            }
+
+            if (Transform.IsArrive(Last, StopingDistance, IgnoreYAxis))
+            {
+                MyAgent.MoveInput(Vector3.zero);
+                GetLogger()?.WriteLine($"MoveTo Succeeded: {Last}");
+                return Status.Succeeded;
+            }
+            else
+            {
+                var dir = Last - Transform.position;
+                MyAgent.MoveInput(dir);
+            }
+
+            return Status.Running;
+        }
+    }
+}
---
+++ UnityAI/Packages/com.megumin.ai.behaviortree/Runtime/Gameplay/Patrol/PatrolBase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Megumin.Binding;
+using UnityEngine;
+
+namespace Megumin.AI.BehaviorTree
+{
+    /// <summary>
+    /// 巡逻基类
+    /// 每到达一个检查点，执行一次子节点。
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class PatrolBase<T, P> : StateChild0<T>
+        where P : PatrolPath<Vector3>
+    {
+        [Space]
+        public float StopingDistance = 0.25f;
+        public bool IgnoreYAxis = true;
+
+        /// <summary>
+        /// 巡逻路径
+        /// </summary>
+        [Space]
+        public P PatrolPath;
+
+        protected Vector3 startPosition;
+        protected override void OnEnter(object options = null)
+        {
+            base.OnEnter(options);
+            startPosition = Transform.position;
+            PatrolPath.StartPoint = startPosition;
+        }
+
+        protected Vector3 destination;
+    }
+}
+
