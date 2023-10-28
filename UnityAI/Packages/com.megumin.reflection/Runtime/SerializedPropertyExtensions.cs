90a949ccf49836757e65fde07a1ca64920f9d283
云却 <479813005@qq.com>
2023-10-28 15:05:21 +0800

    增加反射扩展

--- UnityAI/Packages/com.megumin.reflection/Runtime/SerializedPropertyExtensions.cs
+++ UnityAI/Packages/com.megumin.reflection/Runtime/SerializedPropertyExtensions.cs
@@ -1,10 +1,13 @@
-// <author>
-//   douduck08: https://github.com/douduck08
-//   Use Reflection to get instance of Unity's SerializedProperty in Custom Editor.
-//   Modified codes from 'Unity Answers', in order to apply on nested List<T> or Array. 
-//   
+﻿// <author>
+//   KumoKyaku: https://github.com/KumoKyaku
+//
 //   Original author: HiddenMonk & Johannes Deml
 //   Ref: http://answers.unity3d.com/questions/627090/convert-serializedproperty-to-custom-class.html
+//
+//   Original author: douduck08: https://github.com/douduck08
+//   https://gist.github.com/douduck08/6d3e323b538a741466de00c30aa4b61f
+//   Use Reflection to get instance of Unity's SerializedProperty in Custom Editor.
+//   Modified codes from 'Unity Answers', in order to apply on nested List<T> or Array. 
 // </author>
 
 #if UNITY_EDITOR
@@ -19,12 +22,56 @@ using UnityEngine;
 
 namespace Megumin
 {
-    /// <summary>
-    /// https://gist.github.com/douduck08/6d3e323b538a741466de00c30aa4b61f
-    /// </summary>
     public static class SerializedPropertyExtensions_B4C8CAE8AAFD410981DE4CCC2553F15F
     {
         static readonly Regex rgx = new Regex(@"\[\d+\]");
+
+        /// <summary>
+        /// 获取值的所属对象
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        public static object GetOwnerObject(this SerializedProperty property)
+        {
+            object obj = property.serializedObject.targetObject;
+            string path = property.propertyPath.Replace(".Array.data", "");
+            string[] fieldStructure = path.Split('.');
+
+            for (int i = 0; i < fieldStructure.Length - 1; i++)
+            {
+                if (fieldStructure[i].Contains("["))
+                {
+                    int index = System.Convert.ToInt32(new string(fieldStructure[i].Where(c => char.IsDigit(c)).ToArray()));
+                    obj = GetFieldValueWithIndex(rgx.Replace(fieldStructure[i], ""), obj, index);
+                }
+                else
+                {
+                    obj = GetFieldValue(fieldStructure[i], obj);
+                }
+            }
+            return obj;
+        }
+
+        /// <summary>
+        /// 获取值的所属对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="property"></param>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static bool TryGetOwnerObject<T>(this SerializedProperty property, out T obj)
+        {
+            var owner = GetOwnerObject(property);
+            if (owner is T tObj)
+            {
+                obj = tObj;
+                return true;
+            }
+
+            obj = default;
+            return false;
+        }
+
         public static T GetValue<T>(this SerializedProperty property) where T : class
         {
             object obj = property.serializedObject.targetObject;
