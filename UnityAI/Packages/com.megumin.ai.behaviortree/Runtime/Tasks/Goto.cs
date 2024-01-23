57f702c770d3a9c2c04aaf59378a0609e5dd0e28
云却 <479813005@qq.com>
2024-01-23 11:35:45 +0800

    增加goto示例

--- UnityAI/Packages/com.megumin.ai.behaviortree/Runtime/Tasks/Goto.cs
+++ UnityAI/Packages/com.megumin.ai.behaviortree/Runtime/Tasks/Goto.cs
@@ -13,19 +13,19 @@ namespace Megumin.AI.BehaviorTree
 {
     //需要绘制引用节点UI Ref需要扩展
     // Goto 节点，大杀器，但是极容易导致运行时bug
-    //[Serializable]
-    //[DisplayName("Goto")]
-    //[Category("Custom")]
-    ////[AddComponentMenu("Goto")]
-    ////[Color(1f, 47f / 51f, 0.0156862754f, 1f)]
-    ////[Icon("")]
-    ////[SerializationAlias("Megumin.AI.BehaviorTree.Goto")]
-    ////[MovedFrom(true, sourceNamespace: "Megumin.AI.BehaviorTree", sourceAssembly: null, sourceClassName: "Goto")]
-    ////[HelpURL(URL.WikiDecorator + "Goto")]
-    //public sealed class Goto : BTActionNode
-    //{
-    //    public RefVar<BTNode> target;
-    //}
+    [Serializable]
+    [DisplayName("Goto")]
+    [Category("Custom")]
+    //[AddComponentMenu("Goto")]
+    //[Color(1f, 47f / 51f, 0.0156862754f, 1f)]
+    //[Icon("")]
+    //[SerializationAlias("Megumin.AI.BehaviorTree.Goto")]
+    //[MovedFrom(true, sourceNamespace: "Megumin.AI.BehaviorTree", sourceAssembly: null, sourceClassName: "Goto")]
+    //[HelpURL(URL.WikiDecorator + "Goto")]
+    public sealed class Goto : BTActionNode
+    {
+        //public BTNode target;
+    }
 }
 
 
