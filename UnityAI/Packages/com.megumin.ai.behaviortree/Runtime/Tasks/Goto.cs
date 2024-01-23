99e999885e7c984ba5cf3f115541db63edff309b
云却 <479813005@qq.com>
2024-01-23 16:41:20 +0800

    goto节点

--- UnityAI/Packages/com.megumin.ai.behaviortree/Runtime/Tasks/Goto.cs
+++ UnityAI/Packages/com.megumin.ai.behaviortree/Runtime/Tasks/Goto.cs
@@ -14,24 +14,29 @@ namespace Megumin.AI.BehaviorTree
     //需要绘制引用节点UI Ref需要扩展
 
     /// <summary>
-    /// Goto 节点，大杀器，极大程度弥补行为树的不足。
+    /// Goto 节点。
+    /// 就像目标节点是自己的子节点一样。
+    /// 大杀器，极大程度弥补行为树的不足。
     /// 部分等价与菱形环形结构，突破了树形结构的限制。
     /// 但是也是混乱的开端。潘多拉魔盒。极容易导致运行时bug。
     /// 尽可能小心使用这个节点。
     /// </summary>
     [Serializable]
     [DisplayName("Goto")]
-    [Category("Custom")]
-    //[AddComponentMenu("Goto")]
+    [Category("Utility")]
+    [AddComponentMenu("Goto")]
     //[Color(1f, 47f / 51f, 0.0156862754f, 1f)]
     //[Icon("")]
     //[SerializationAlias("Megumin.AI.BehaviorTree.Goto")]
     //[MovedFrom(true, sourceNamespace: "Megumin.AI.BehaviorTree", sourceAssembly: null, sourceClassName: "Goto")]
-    //[HelpURL(URL.WikiDecorator + "Goto")]
-    public sealed class Goto : BTActionNode
+    [HelpURL(URL.WikiTask + "Goto")]
+    public sealed class Goto : BTActionNode, IDataValidable, IDetailable
     {
+        public int Test;
         [RefSetter]
         public BTNode target;
+        public int Test2;
+        public RefVar_Float test3;
 
         protected override Status OnTick(BTNode from, object options = null)
         {
@@ -41,6 +46,25 @@ namespace Megumin.AI.BehaviorTree
             }
 
             return GetIgnoreResult(from);
+        }
+
+        public (int Result, string ToolTip) Valid()
+        {
+            if (target == null)
+            {
+                return (-1, "no target, ignore this");
+            }
+
+            return (0, "");
+        }
+
+        public string GetDetail()
+        {
+            if (target != null)
+            {
+                return $"Target: {target.ShortGUID}";
+            }
+            return null;
         }
     }
 }
